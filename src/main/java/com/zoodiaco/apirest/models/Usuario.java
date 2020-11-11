4ebd90b5786a02b3e133be42be4f0434d57eb3b7
Tiago <tiagodrade18@gmail.com>
2020-11-11 01:20:52 -0300

    init Commit

---
+++ src/main/java/com/zoodiaco/apirest/models/Usuario.java
@@ -0,0 +1,62 @@
+package com.zoodiaco.apirest.models;
+import java.io.Serializable;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="tb_usuario")
+public class Usuario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String nome;
+	
+	private int premium;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getPremium() {
+		return premium;
+	}
+
+	public void setPremium(int premium) {
+		this.premium = premium;
+	}
+
+	
+	
+	
+	
+}
+
+
+
+
+
+
+	
+	
--- src/main/java/com/zoodiaco/apirest/resources/ZodiacoResource.java
+++ src/main/java/com/zoodiaco/apirest/resources/ZodiacoResource.java
@@ -5,9 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.zoodiaco.apirest.models.Usuario;
 import com.zoodiaco.apirest.models.Zodiaco;
+import com.zoodiaco.apirest.repository.UsuarioRepository;
 import com.zoodiaco.apirest.repository.ZodiacoRepository;
 
 
@@ -20,6 +25,9 @@ public class ZodiacoResource {
 	@Autowired
 	ZodiacoRepository zodiacoRepository;
 	
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
 	//Listar zodiacos salvos
 	@GetMapping("/zodiacos")
 	public List<Zodiaco> listaZodiacos(){
@@ -27,8 +35,13 @@ public class ZodiacoResource {
 	}
 	
 	
-		@GetMapping("/zodiacos/{id}")
-		public Zodiaco listaUmZodiaco(@PathVariable(value="id") long id){
-			return zodiacoRepository.findById(id);
-		}
+	@GetMapping("/zodiacos/{id}")
+	public Zodiaco listaUmZodiaco(@PathVariable(value="id") long id){
+		return zodiacoRepository.findById(id);
+	}
+		
+	@PostMapping("/cadastrar")
+	public Usuario salvaUsuario(@RequestBody Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
 }
